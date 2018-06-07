3d0be9eb44adb2695b3417c0de8ddde7f17fdc42
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-06-07 12:01:30 -0600

    add map

--- components/MapHome.js
+++ components/MapHome.js
@@ -1,19 +1,84 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
+import { MapView } from 'expo';
+import MapStyle from './MapStyle.json'
 
-const MapHome =() => (
-      <View style={styles.container}>
-          <Text>Hello</Text>
-      </View>
+export default class MapHome extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mapRegion: null,
+      hasLocationPermissions: false,
+      locationResult: null,
+      currentUser: this.props.navigation.state.params.userName,
+      userPicture: this.props.navigation.state.params.userPic,
+    }
+  }
+
+  _handleMapRegionChange = mapRegion => {
+    this.setState({ mapRegion });
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        locationResult: 'Permission to access location was denied',
+      });
+    } else {
+      this.setState({ hasLocationPermissions: true })
+    }
+
+    let location = await Location.getCurrentPositionAsync({})
+    this.setState({ locationResult: JSON.stringify(location) })
+
+    this.setState({
+      mapRegion: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }
+    })
+  }
+
+  componentDidMount() {
+    this._getLocationAsync()
+  }
+
+  render() {
+    return (
+
+      <MapView
+        provider={Expo.MapView.PROVIDER_GOOGLE}
+        customMapStyle={MapStyle}
+        style={{ flex: 1 }}
+        region={this.state.mapRegion}
+        onRegionChange={this._handleMapRegionChange}
+        showsUserLocation={true}
+        zoomEnabled={true}
+        pitchEnabled={true}
+        showsUserLocation={true}
+        followsUserLocation={true}
+      >
+      </MapView>
+
     );
-
-export default  MapHome;
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
