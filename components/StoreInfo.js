4566dcc96d85c1aa3c1da6b6817d69f4c074fa42
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-06-11 11:04:56 -0600

    add stars

--- components/StoreInfo.js
+++ components/StoreInfo.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
-
-
-
-
+import Stars from 'react-native-stars';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 export default class StoreInfo extends Component {
@@ -17,13 +15,41 @@ export default class StoreInfo extends Component {
     render() {
         console.log(this.props.store_id, "store ID info page");
         console.log(this.props.storeData, "All stores info page");
-        
+
+        const storeData = this.props.storeData;
+        const selectedStore = this.props.store_id;
+        const singleStore = storeData.filter(store => {
+            if (store.store_id === selectedStore) {
+                return store
+            }
+        })[0]
+
+        console.log(singleStore && singleStore.name, "store name");
+
         return (
             <View
                 style={styles.container}
             >
-            <Text>Store Info Page</Text>
-
+                <Image
+                    style={{ width: 300, height: 200 }}
+                    source={{ uri: singleStore && singleStore.photo }}
+                />
+                <Text>{singleStore && singleStore.name}</Text>
+                <Text>{singleStore && singleStore.address}</Text>
+                <Text>{singleStore && singleStore.hours}</Text>
+                <Text>{singleStore && singleStore.web_address}</Text>
+                <Text>{singleStore && singleStore.facebook}</Text>
+                <Text>{singleStore && singleStore.description}</Text>
+                <Text>{singleStore && singleStore.average_rating}</Text>
+                <View style={{ alignItems: 'center' }}>
+                    <Stars
+                        value={singleStore && singleStore.average_rating}
+                        spacing={8}
+                        count={5}
+                        starSize={30}
+                        backingColor='#ffffff'
+                        />
+                </View>
             </View>
         )
     };
@@ -32,12 +58,9 @@ export default class StoreInfo extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#2e4366',
+        backgroundColor: '#ffffff',
         alignItems: 'center',
         justifyContent: 'center',
-        // height: 100,
-        // width: 100
-
     },
 });
 
